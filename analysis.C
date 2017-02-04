c05e2f15fa6209e0fe75e52cabf217fbe5d189da
ljeure <lukee@mit.edu>
2017-02-04 13:21:29 -0500

    added plots for homework

---
+++ analysis.C
@@ -0,0 +1,104 @@
+#define analysis_cxx
+#include "analysis.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <iostream>
+#include <map>
+
+void analysis::Loop()
+{
+//   In a ROOT session, you can do:
+//      root> .L analysis.C
+//      root> analysis t
+//      root> t.GetEntry(12); // Fill t data members with entry number 12
+//      root> t.Show();       // Show values of entry 12
+//      root> t.Show(16);     // Read and show values of entry 16
+//      root> t.Loop();       // Loop on all entries
+//
+
+//     This is the loop skeleton where:
+//    jentry is the global entry number in the chain
+//    ientry is the entry number in the current Tree
+//  Note that the argument to GetEntry must be:
+//    jentry for TChain::GetEntry
+//    ientry for TTree::GetEntry and TBranch::GetEntry
+//
+//       To read only selected branches, Insert statements like:
+// METHOD1:
+//    fChain->SetBranchStatus("*",0);  // disable all branches
+//    fChain->SetBranchStatus("branchname",1);  // activate branchname
+// METHOD2: replace line
+//    fChain->GetEntry(jentry);       //read all branches
+//by  b_branchname->GetEntry(ientry); //read only this branch
+   if (fChain == 0) return;
+
+   TFile * outputfile = new TFile("outfilename.root", "recreate");
+   TH1D * htrkPt = new TH1D("htrkPt", 
+       "average track momentum;number of collisions;average track momentum",
+       100,0.2,100);
+   TH1D * htrkPt2 = new TH1D("htrkPt2", 
+       "track momentum v. number of tracks;number of tracks in event; mean track momentum *10",
+       100,0.2,100);
+
+   Long64_t nentries = fChain->GetEntriesFast();
+
+   Long64_t nbytes = 0, nb = 0;
+
+   // for avg track momentum v # tracks in event
+   std::map <int, double> sum_avg_momentum;
+   std::map <int, int> entries_avg_momentum;
+
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+
+      if (nTrk < 1) continue;
+
+      Long64_t total_track_Pt = 0;
+      for (int itrk = 0 ; itrk< nTrk ; itrk++) {
+        total_track_Pt += trkPt[itrk];
+      }
+      Long64_t avg_track_Pt = total_track_Pt / nTrk;
+
+      htrkPt->Fill(avg_track_Pt);
+
+      // track mometum changing as function of number of tracks
+      Long64_t total_track_Pt2 = 0;
+      for (int itrk = 0 ; itrk< nTrk ; itrk++) {
+        total_track_Pt2 += trkPt[itrk];
+      }
+      Long64_t avg_track_Pt2 = total_track_Pt2 / nTrk;
+
+      // if map entry doesn't exist for this number of tracks, create it
+      if ( sum_avg_momentum.find(nTrk) == sum_avg_momentum.end() ) {
+        sum_avg_momentum[nTrk] = avg_track_Pt2;
+        entries_avg_momentum[nTrk] = 1;
+      }
+      else {
+        sum_avg_momentum[nTrk] += avg_track_Pt2;
+        entries_avg_momentum[nTrk] += 1;
+      }
+   }
+
+   for(std::map<int,double>::iterator iter = sum_avg_momentum.begin(); iter != sum_avg_momentum.end(); ++iter)
+   {
+     int k =  iter->first;
+     if (k==3) continue;
+     int avg_times_10 = (int) (sum_avg_momentum[k] / entries_avg_momentum[k] * 10);
+     for (int i = 0; i < avg_times_10; i++) {
+       htrkPt2->Fill(k);
+     }
+   }
+
+   outputfile->Write();
+   outputfile->Close();
+}
+
+int main(int argc, char *argv[])
+{
+  analysis * ana = new analysis();
+  ana->Loop();
+  return 0;
+}
---
+++ analysis.h
@@ -0,0 +1,195 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Tue Jan 24 14:08:23 2017 by ROOT version 6.08/02
+// from TTree ztree/Jet track tree
+// found on file: g.pp-photonHLTFilter-v0-HiForest-tutorial.root
+//////////////////////////////////////////////////////////
+
+#ifndef analysis_h
+#define analysis_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+// Header file for the classes stored in the TTree if any.
+
+class analysis {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+   // Declaration of leaf types
+   Int_t           isPP;
+   UInt_t          run;
+   ULong64_t       evt;
+   UInt_t          lumi;
+   Float_t         vz;
+   Int_t           njet;
+   Float_t         jetpt[5];   //[njet]
+   Float_t         jetptCorr[5];   //[njet]
+   Float_t         jeteta[5];   //[njet]
+   Float_t         jetphi[5];   //[njet]
+   Int_t           nTrk;
+   Float_t         trkPt[95];   //[nTrk]
+   Float_t         trkEta[95];   //[nTrk]
+   Float_t         trkPhi[95];   //[nTrk]
+   Int_t           trkCharge[95];   //[nTrk]
+   Bool_t          highPurity[95];   //[nTrk]
+   Float_t         trkWeight[95];   //[nTrk]
+   Int_t           nPho;
+   Float_t         phoE[1];   //[nPho]
+   Float_t         phoEt[1];   //[nPho]
+   Float_t         phoEtCorrected[1];   //[nPho]
+   Float_t         phoEta[1];   //[nPho]
+   Float_t         phoPhi[1];   //[nPho]
+
+   // List of branches
+   TBranch        *b_isPP;   //!
+   TBranch        *b_run;   //!
+   TBranch        *b_evt;   //!
+   TBranch        *b_lumi;   //!
+   TBranch        *b_vz;   //!
+   TBranch        *b_njet;   //!
+   TBranch        *b_jetpt;   //!
+   TBranch        *b_jetptCorr;   //!
+   TBranch        *b_jeteta;   //!
+   TBranch        *b_jetphi;   //!
+   TBranch        *b_nTrk;   //!
+   TBranch        *b_trkPt;   //!
+   TBranch        *b_trkEta;   //!
+   TBranch        *b_trkPhi;   //!
+   TBranch        *b_trkCharge;   //!
+   TBranch        *b_highPurity;   //!
+   TBranch        *b_trkWeight;   //!
+   TBranch        *b_nPho;   //!
+   TBranch        *b_phoE;   //!
+   TBranch        *b_phoEt;   //!
+   TBranch        *b_phoEtCorrected;   //!
+   TBranch        *b_phoEta;   //!
+   TBranch        *b_phoPhi;   //!
+
+   analysis(TTree *tree=0);
+   virtual ~analysis();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+};
+
+#endif
+
+#ifdef analysis_cxx
+analysis::analysis(TTree *tree) : fChain(0) 
+{
+// if parameter tree is not specified (or zero), connect the file
+// used to generate this class and read the Tree.
+   if (tree == 0) {
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("g.pp-photonHLTFilter-v0-HiForest-tutorial.root");
+      if (!f || !f->IsOpen()) {
+         f = new TFile("g.pp-photonHLTFilter-v0-HiForest-tutorial.root");
+      }
+      f->GetObject("ztree",tree);
+
+   }
+   Init(tree);
+}
+
+analysis::~analysis()
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+}
+
+Int_t analysis::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+Long64_t analysis::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void analysis::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("isPP", &isPP, &b_isPP);
+   fChain->SetBranchAddress("run", &run, &b_run);
+   fChain->SetBranchAddress("evt", &evt, &b_evt);
+   fChain->SetBranchAddress("lumi", &lumi, &b_lumi);
+   fChain->SetBranchAddress("vz", &vz, &b_vz);
+   fChain->SetBranchAddress("njet", &njet, &b_njet);
+   fChain->SetBranchAddress("jetpt", jetpt, &b_jetpt);
+   fChain->SetBranchAddress("jetptCorr", jetptCorr, &b_jetptCorr);
+   fChain->SetBranchAddress("jeteta", jeteta, &b_jeteta);
+   fChain->SetBranchAddress("jetphi", jetphi, &b_jetphi);
+   fChain->SetBranchAddress("nTrk", &nTrk, &b_nTrk);
+   fChain->SetBranchAddress("trkPt", trkPt, &b_trkPt);
+   fChain->SetBranchAddress("trkEta", trkEta, &b_trkEta);
+   fChain->SetBranchAddress("trkPhi", trkPhi, &b_trkPhi);
+   fChain->SetBranchAddress("trkCharge", trkCharge, &b_trkCharge);
+   fChain->SetBranchAddress("highPurity", highPurity, &b_highPurity);
+   fChain->SetBranchAddress("trkWeight", trkWeight, &b_trkWeight);
+   fChain->SetBranchAddress("nPho", &nPho, &b_nPho);
+   fChain->SetBranchAddress("phoE", phoE, &b_phoE);
+   fChain->SetBranchAddress("phoEt", phoEt, &b_phoEt);
+   fChain->SetBranchAddress("phoEtCorrected", phoEtCorrected, &b_phoEtCorrected);
+   fChain->SetBranchAddress("phoEta", phoEta, &b_phoEta);
+   fChain->SetBranchAddress("phoPhi", phoPhi, &b_phoPhi);
+   Notify();
+}
+
+Bool_t analysis::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void analysis::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t analysis::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+#endif // #ifdef analysis_cxx
